e45a609039283fe6b1e77a455ea3a334e4d8ffce
Bosian2000 <basinchina@gmail.com>
2025-06-08 15:49:33 +0800

    🚑 Fix: remove stray backslash in import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-\import { useState } from 'react'
+import { useState } from 'react'
 import axios from 'axios'
 import Rating from '../components/Rating'
 import { submitFeedback } from '../lib/feedback'

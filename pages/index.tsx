ea673488b92e95288f8e506ed1cd0e1267899817
Bosian2000 <basinchina@gmail.com>
2025-06-09 12:39:54 +0800

    Fix import path for MenuDisplay to resolve Vercel build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Head from 'next/head';
-import MenuDisplay from '@/components/MenuDisplay';
+import MenuDisplay from '../components/MenuDisplay';
 
 type Role = 'user' | 'assistant';
 
